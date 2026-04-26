9c8bed15e82db32bd0b7c2eaea9157f3dc39beb2
StevinoTR <ps8.ramanandraystevino@gmail.com>
2026-04-26 23:03:04 +0300

    modifn

--- backend/models/Reservation.js
+++ backend/models/Reservation.js
@@ -1,29 +1,56 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 const User = require('./User');
-const Car = require('./Car');
+const Car  = require('./Car');
 
 const Reservation = sequelize.define('Reservation', {
   id: {
-    type: DataTypes.INTEGER.UNSIGNED,
+    type: DataTypes.BIGINT.UNSIGNED,
     primaryKey: true,
     autoIncrement: true
   },
-  voitureId: { type: DataTypes.INTEGER.UNSIGNED, allowNull: false },
-  clientId:  { type: DataTypes.INTEGER.UNSIGNED, allowNull: false },
+  voitureId: {
+    type:      DataTypes.BIGINT.UNSIGNED,
+    allowNull: false,
+    field:     'voiture_id'
+  },
+  clientId: {
+    type:      DataTypes.BIGINT.UNSIGNED,
+    allowNull: false,
+    field:     'client_id'
+  },
   date_debut: { type: DataTypes.DATEONLY, allowNull: false },
   date_fin:   { type: DataTypes.DATEONLY, allowNull: false },
-  statut: { type: DataTypes.ENUM('en_attente','confirmee','annulee','terminee'), defaultValue: 'en_attente' },
-  montant:  DataTypes.INTEGER.UNSIGNED,
-  reference:{ type: DataTypes.STRING, unique: true }
+  statut:     { type: DataTypes.STRING,   allowNull: false, defaultValue: 'en_attente' },
+  montant:    { type: DataTypes.INTEGER,  allowNull: true  },
+  reference:  { type: DataTypes.STRING,   unique: true     },
+  nb_jours:   { type: DataTypes.INTEGER,  allowNull: true  },
+  nom_client:   { type: DataTypes.STRING, allowNull: true  },
+  email_client: { type: DataTypes.STRING, allowNull: true  },
+  tel_client:   { type: DataTypes.STRING, allowNull: true  },
+  cin_client:   { type: DataTypes.STRING, allowNull: true  },
+  lieu:         { type: DataTypes.STRING, allowNull: true  },
+  message:      { type: DataTypes.TEXT,   allowNull: true  }
 }, {
-  tableName: 'reservations',
+  tableName:  'reservations',
   timestamps: true
 });
 
-Reservation.belongsTo(User, { as: 'client', foreignKey: 'clientId' });
-Reservation.belongsTo(Car, { as: 'voiture', foreignKey: 'voitureId' });
-Car.hasMany(Reservation, { as: 'reservations', foreignKey: 'voitureId' });
-User.hasMany(Reservation, { as: 'reservations', foreignKey: 'clientId' });
+Reservation.belongsTo(User, {
+  as:         'client',
+  foreignKey: { name: 'clientId',  field: 'client_id'  }
+});
+Reservation.belongsTo(Car, {
+  as:         'voiture',
+  foreignKey: { name: 'voitureId', field: 'voiture_id' }
+});
+Car.hasMany(Reservation, {
+  as:         'reservations',
+  foreignKey: { name: 'voitureId', field: 'voiture_id' }
+});
+User.hasMany(Reservation, {
+  as:         'reservations',
+  foreignKey: { name: 'clientId',  field: 'client_id'  }
+});
 
 module.exports = Reservation;
